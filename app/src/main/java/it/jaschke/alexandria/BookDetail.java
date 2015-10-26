f8862975a727c9cf9df9518c3ebcd64224e3d7d2
mndivya <nagadivya.m@gmail.com>
2015-10-26 17:20:07 -0400

    Added RightContainer for tablets

--- app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -122,7 +122,7 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
         ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
 
-        if(rootView.findViewById(R.id.right_container)!=null){
+        if(getActivity().findViewById(R.id.right_container)!=null){
             rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
         }
 
@@ -138,8 +138,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         super.onDestroyView();
 
         //Todo divya
-//        if(MainActivity.IS_TABLET && rootView.findViewById(R.id.right_container)==null){
-//            getActivity().getSupportFragmentManager().popBackStack();
-//        }
+        if(MainActivity.IS_TABLET && getActivity().findViewById(R.id.right_container)==null){
+            getActivity().getSupportFragmentManager().popBackStack();
+        }
     }
 }
